4f6373645a487c0be5a1348d4402686d820a3c3a
Frisaf <fridasafsten@gmail.com>
2024-08-28 09:33:27 +0200

    edit

--- lektion-1.py
+++ lektion-1.py
@@ -9,3 +9,5 @@ print(f"{important}, and that is important! My name is {name} by the way.")
 q = input("What is your name, then?\n> ")
 
 print(f"Your name is {q}, and you should also remember that {important.lower()}")
+
+print(type(name))
