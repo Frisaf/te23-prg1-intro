be1c4c51d0d429d564dca9d6cc2468c5e603d88f
Frisaf <fridasafsten@gmail.com>
2024-08-28 09:31:38 +0200

    initial commit

---
+++ lektion-1.py
@@ -0,0 +1,11 @@
+name = "Ash"
+print(name)
+print(type(name))
+
+important = "We use quotation marks for strings."
+
+print(f"{important}, and that is important! My name is {name} by the way.")
+
+q = input("What is your name, then?\n> ")
+
+print(f"Your name is {q}, and you should also remember that {important.lower()}")
