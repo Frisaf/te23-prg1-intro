75648154dccb0d8abcdf4a6dd03cfbbe8dad21bc
Frisaf <fridasafsten@gmail.com>
2024-11-26 11:57:26 +0100

    111 kronor

--- 111_kr.py
+++ 111_kr.py
@@ -2,7 +2,22 @@
 import sys
 
 for line in sys.stdin:
-    data = line
+    data = int(line)
 
     one = "1"
+
+    digits = len(str(data))
+    bills = 0
     
+
+    while True:
+        number = digits * one
+        division = int(data/int(number))
+        data -= division * int(number)
+        digits -= 1
+        bills += division
+
+        if not data % int(number) != 0:
+            break
+
+    print(bills)
---
+++ test.py
@@ -0,0 +1,4 @@
+a = 5.6
+b = int(a)
+
+print(b)
